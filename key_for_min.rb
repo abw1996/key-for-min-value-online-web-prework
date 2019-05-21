b362f258bb5f432ff0675da95829e59f31f25840
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-05-21 04:25:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,8 @@
 
 def key_for_min_value(name_hash)
   hashKey = 100000
-  something = 0
-  name_hash.collect 
+  something = ""
+  name_hash.collect do |starter, pack|
+    if hashKey > pack
+      something = starter
 end
