c53d61df16a66b891947d6e1dd73b8d16f23f97f
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-05-21 04:02:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  hashKey = name_hash9
-  fame = name_hash.collect do |key, value|
-    value
-  end
+  hashKey = name_hash(0)
+  
 end
